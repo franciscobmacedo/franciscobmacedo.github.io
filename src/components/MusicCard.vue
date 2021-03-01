94f9f50ca3cb31fa3b6f2ba8b8d656116f2a1e6d
firstmanonthemoon <40719025+firstmanonthemoon@users.noreply.github.com>
2021-03-01 22:59:00 +0000

    with first skeletons

--- src/components/MusicCard.vue
+++ src/components/MusicCard.vue
@@ -1,36 +1,55 @@
 <template>
-<v-col cols="12" sm="6" md="4" lg="3">
-     <v-card outlined tile
+  <v-col cols="12" sm="6" md="4" lg="3">
+    <v-skeleton-loader
+      v-if="loading"
+      :loading="loading"
+      transition="fade-transition"
+      type="card"
+    >
+    </v-skeleton-loader>
 
-     class="text-center"
-     >
-          <div class="text-overline pl-3 pr-3 "
-          style="height: 60px"
-          
-          >
-            {{music.name}}
-          </div>
-          <vuetify-audio  flat :id="music.id" :file="music.file" color="secondary"  ></vuetify-audio>
-        </v-card>
-        </v-col>
+    <v-card outlined tile class="text-center" v-show="!loading">
+      <div class="text-overline pl-3 pr-3" style="height: 60px">
+        {{ music.name }}
+      </div>
+      <vuetify-audio
+        flat
+        :id="music.id"
+        :file="music.file"
+        color="secondary"
+      ></vuetify-audio>
+    </v-card>
+  </v-col>
 </template>
 
 <script>
-import VuetifyAudio from "@/components/VuetifyAudio"
+import VuetifyAudio from "@/components/VuetifyAudio";
 export default {
-    props:{
-        music: Object,
-    },
-    components:{
-        VuetifyAudio
-    },
-    methods:{
-    
-    }
+  props: {
+    music: Object,
+  },
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  components: {
+    VuetifyAudio,
+  },
+  created() {
+    const readyHandler = () => {
+      if (document.readyState == "complete") {
+        this.loading = false;
+        document.removeEventListener("readystatechange", readyHandler);
+      }
+    };
 
-}
+    document.addEventListener("readystatechange", readyHandler);
+
+    readyHandler(); // in case the component has been instantiated lately after loading
+  },
+};
 </script>
 
 <style>
-
 </style>
