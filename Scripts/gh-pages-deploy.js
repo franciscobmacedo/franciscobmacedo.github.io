eedb78ed1c957435a8770b64408284794fe785e1
Francisco Macedo <franciscomacedo@lisboaenova.org>
2021-03-02 14:19:23 +0000

    update scripts!

--- Scripts/gh-pages-deploy.js
+++ Scripts/gh-pages-deploy.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-console */
 const execa = require("execa");
 var rimraf = require("rimraf");
-
-
 const fs = require("fs");
+
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
@@ -12,12 +11,13 @@ const fs = require("fs");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await fs.writeFile(`${folderName}/CNAME`, "franciscomacedo.pt", function(err) {console.log(err)});
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     // await execa("rm", ["-r", folderName]);
-    await rimraf("/some/directory", function () { console.log("done"); });
+    await rimraf(folderName, function () { console.log("removed folder"); });
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
